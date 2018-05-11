a4557100baeee01142ccb28492b45c9fcdbeedfb
DESKTOP-P1ABHVA\kdh95 <kdh950812@gmail.com>
2018-05-11 18:16:00 +0900

    장기 프로젝트 추가 : 맵, 유닛 배치 추가

---
+++ jangi/Assets/Scripts/MNG/GameMNG.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UNIT {
+	public int y, x;
+	public GameObject unitObject;
+	public Unit unitScript;
+	public EUNITTYPE unitType;
+
+	public UNIT(GameObject unitObject, int y, int x, EUNITTYPE eType, int team) {
+		this.unitObject = unitObject;
+		this.y = y;
+		this.x = x;
+		unitType = eType;
+		unitScript = unitObject.GetComponent<Unit>();
+		unitScript.Create(y, x, eType, team);
+	}
+};
+
+public class GameMNG : MonoBehaviour {
+
+	public int[,] map = new int[10,9];
+	UNIT[] units = new UNIT[32];
+	GameObject unitPrefab;
+
+	void Awake() {
+		unitPrefab = (GameObject)Resources.Load("Prefabs/Unit/Unit");
+	}
+	// Use this for initialization
+	void Start () {
+		for(int i=0; i<10; i++) {
+			for(int j=0; j<9; j++)
+				map[i,j] = 0;
+		}
+
+		int count = 0;
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 0, 0, EUNITTYPE.E_CAR, 0);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 0, 1, EUNITTYPE.E_HOR, 0);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 0, 2, EUNITTYPE.E_COW, 0);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 0, 3, EUNITTYPE.E_SA, 0);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 0, 5, EUNITTYPE.E_SA, 0);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 0, 6, EUNITTYPE.E_COW, 0);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 0, 7, EUNITTYPE.E_HOR, 0);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 0, 8, EUNITTYPE.E_CAR, 0);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 1, 4, EUNITTYPE.E_KING, 0);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 2, 1, EUNITTYPE.E_TNK, 0);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 2, 8, EUNITTYPE.E_TNK, 0);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 3, 0, EUNITTYPE.E_SOL, 0);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 3, 2, EUNITTYPE.E_SOL, 0);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 3, 4, EUNITTYPE.E_SOL, 0);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 3, 6, EUNITTYPE.E_SOL, 0);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 3, 8, EUNITTYPE.E_SOL, 0);
+
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 9, 0, EUNITTYPE.E_CAR, 1);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 9, 1, EUNITTYPE.E_HOR, 1);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 9, 2, EUNITTYPE.E_COW, 1);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 9, 3, EUNITTYPE.E_SA, 1);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 9, 5, EUNITTYPE.E_SA, 1);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 9, 6, EUNITTYPE.E_COW, 1);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 9, 7, EUNITTYPE.E_HOR, 1);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 9, 8, EUNITTYPE.E_CAR, 1);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 8, 4, EUNITTYPE.E_KING, 1);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 7, 1, EUNITTYPE.E_TNK, 1);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 7, 8, EUNITTYPE.E_TNK, 1);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 6, 0, EUNITTYPE.E_SOL, 1);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 6, 2, EUNITTYPE.E_SOL, 1);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 6, 4, EUNITTYPE.E_SOL, 1);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 6, 6, EUNITTYPE.E_SOL, 1);
+		units[count++] = new UNIT((GameObject)Instantiate(unitPrefab), 6, 8, EUNITTYPE.E_SOL, 1);
+
+		for(int i=0; i < count; i++) {
+			map[units[i].y, units[i].x] = (int)units[i].unitType;
+			units[i].unitObject.transform.parent = transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ jangi/Assets/Scripts/Unit/Unit.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EUNITTYPE {
+	E_SOL,
+	E_CAR,
+	E_HOR,
+	E_COW,
+	E_TNK,
+	E_SA,
+	E_KING
+};
+
+public class Unit : MonoBehaviour {
+
+	int y, x;
+	public int team;
+	Vector3 position;
+	public EUNITTYPE unitType;
+	TextMesh unitName;
+
+	void Awake() {
+		unitName = transform.GetChild(0).GetComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Create(int y, int x, EUNITTYPE eType, int team) {
+		this.y = y;
+		this.x = x;
+		unitType = eType;
+
+		position.x = (-4f * 1.31f) + (x * 1.31f);
+		position.y = (3.81f) + (0.85f * -y);
+		transform.position = position;
+
+		this.team = team;
+		if(team == 0) 
+			unitName.color = Color.red;
+		else
+			unitName.color = Color.blue;
+
+		SetName(eType);
+	}
+
+	void SetName(EUNITTYPE eType) {
+		if(eType == EUNITTYPE.E_SOL) 
+			unitName.text = "쫄";
+		if(eType == EUNITTYPE.E_CAR) 
+			unitName.text = "차";
+		if(eType == EUNITTYPE.E_HOR) 
+			unitName.text = "말";
+		if(eType == EUNITTYPE.E_COW) 
+			unitName.text = "소";
+		if(eType == EUNITTYPE.E_TNK) 
+			unitName.text = "포";
+		if(eType == EUNITTYPE.E_SA) 
+			unitName.text = "사";
+		if(eType == EUNITTYPE.E_KING) 
+			unitName.text = "왕";
+	}
+
+	public void SetYX(int y, int x) {
+		this.y = y;
+		this.x = x;
+	}
+}
